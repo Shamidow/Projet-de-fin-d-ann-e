08ab0c9975afc8b180bfa0c3d1967ac1877a4249
EtCtulu <bertrandemilien17@gmail.com>
2020-02-06 13:35:52 +0100

    Ajout de la flashlight

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public GameObject Flashlight;
+    private bool FlashlightE = false;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (FlashlightE == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F)) 
+            { 
+                Flashlight.SetActive(false);
+                FlashlightE = false;
+                return;
+            }
+        }
+        if (FlashlightE == false)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Flashlight.SetActive(true);
+                FlashlightE = true;
+                return;
+            }
+        }
+
+        
+        
+
+
+        
+    }
+}
