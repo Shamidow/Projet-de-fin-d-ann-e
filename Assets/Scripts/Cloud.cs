4cb6f398db93c16f672f262a47d6887afc476da7
EtCtulu <bertrandemilien17@gmail.com>
2020-04-15 17:25:52 +0200

    Ajout de quelques matériaux tests, puis d'une zone qui grossi (Placeholder du nuage) ce nuage s'active pour le moment au contact avec un Placeholder trigger. Par la suite il pourra s'activer a l'aide d'un timer, une idée pas mal serait quand même d'activer le premier avec un trigger pour introduire la mechanique. Prochaine étape -> création des HPS du personnage, puis la perte de vie au contact du Nuage.

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float timer = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.localScale = transform.localScale + new Vector3(0,1 * Time.deltaTime, 0.1*Time.deltaTime, 0.1*Time.deltaTime);            
+    }
+    private void FixedUpdate()
+    {
+        if (timer <= 120f)
+        {
+            transform.localScale = transform.localScale + new Vector3(0.001f, 0.001f, 0.001f);
+            timer = timer + Time.deltaTime;
+            Debug.Log(timer);
+        }
+    }
+
+}
