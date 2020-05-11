64b29a5f30020096f5b4136a46ee6040624860a7
EtCtulu <bertrandemilien17@gmail.com>
2020-05-11 09:47:11 +0200

    Demo Ready ?

--- Assets/Scripts/ParticleDMGtest.cs
+++ Assets/Scripts/ParticleDMGtest.cs
@@ -18,7 +18,7 @@ public class ParticleDMGtest : MonoBehaviour
         if (other.tag == "Player")
         {
             Debug.Log(other.tag);
-            PlayerHP.hp = PlayerHP.hp - 0.05f;
+            PlayerHP.hp = PlayerHP.hp - 0.20f;
             HealthBar.fillAmount = PlayerHP.hp / 100f;
             Debug.Log("Nique ta race" + PlayerHP.hp);
         }
