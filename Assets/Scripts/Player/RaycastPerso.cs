7e4803c1d988ce410dbb20581eaeadf8c42f5afe
EtCtulu <bertrandemilien17@gmail.com>
2020-05-12 10:50:42 +0200

    Nique ta mère unity

--- Assets/Scripts/Player/RaycastPerso.cs
+++ Assets/Scripts/Player/RaycastPerso.cs
@@ -219,10 +219,10 @@ public class RaycastPerso : MonoBehaviour
                         return;
                     }
                 }
-                if(hit.collider.gameObject.CompareTag("Door Labo") && lecteurLabo == false)
+                /*if(hit.collider.gameObject.CompareTag("Door Labo") && lecteurLabo == false)
                 {
                     FindObjectOfType<AudioManager>().Play("Door Locked");
-                }
+                }*/
                 if (hit.collider.gameObject.CompareTag("KeyLabo"))
                 {
                     keyLabo = true;
