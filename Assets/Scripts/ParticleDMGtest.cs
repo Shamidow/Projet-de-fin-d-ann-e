d311685ede9eaed2143ba65aa63e44a77f4a2a23
Shamidow <pannier.thibault@gmail.com>
2020-05-26 11:25:34 +0200

    Random qui joue un toussement toute les 5 sec

--- Assets/Scripts/ParticleDMGtest.cs
+++ Assets/Scripts/ParticleDMGtest.cs
@@ -8,10 +8,14 @@ public class ParticleDMGtest : MonoBehaviour
     public ParticleSystem part;
     public Image HealthBar;
     public Image HealthBarG;
-
+    public GameObject[] Cough;
+    private int randomInt;
+    private int Timer = 8;
     void Start()
     {
         part = GetComponent<ParticleSystem>();
+        randomInt = Random.Range(0, Cough.Length);
+        StartCoroutine(RandomCough());
     }
 
     void OnParticleCollision(GameObject other)
@@ -23,6 +27,36 @@ public class ParticleDMGtest : MonoBehaviour
             HealthBar.fillAmount = PlayerHP.hp / 100f;
             HealthBarG.fillAmount = PlayerHP.hp / 100f;
             Debug.Log("Nique ta race" + PlayerHP.hp);
+
+        }
+    }
+
+    int GetRandom(int count)
+    {
+        return Random.Range(0, count);
+    }
+
+    IEnumerator RandomCough()
+    {
+        while (Timer != 0)
+        {
+            randomInt = GetRandom(Cough.Length);
+           
+
+           Destroy(Instantiate(Cough[randomInt]), 5f);
+            Timer--;
+           
+            yield return new WaitForSeconds(5f);
+        }
+
+        while (Timer <= 0)
+        {
+            randomInt = GetRandom(Cough.Length);
+          
+
+            Destroy(Instantiate(Cough[randomInt]), 5f);
+
+            yield return new WaitForSeconds(5f);
         }
     }
 }
