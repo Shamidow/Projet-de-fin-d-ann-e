422e9584a14d70ad3d87bba05419c71465a1a9b3
EtCtulu <bertrandemilien17@gmail.com>
2020-05-26 10:01:27 +0200

    Bah alors ya des trucs

---
+++ Assets/Scripts/ExtincteurMortLol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtincteurMortLol : MonoBehaviour
+{
+    public float timer = 0f;
+    public GameObject extincteur;
+    public GameObject empty;
+    public GameObject auEx;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnParticleCollider.boole == true)
+        {
+            timer = timer + 1 * Time.deltaTime;
+        }
+        if (timer >= 2f)
+        {
+            extincteur.SetActive(false);
+
+
+
+
+
+
+
+
+
+
+            auEx.SetActive(true);
+            auEx.transform.parent = null;
+
+
+
+
+
+            RaycastPerso.extincteur = false;
+
+            empty.SetActive(false);
+        }
+    }
+}
