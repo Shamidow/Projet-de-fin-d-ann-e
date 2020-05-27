b50adba918a1a4392ceaf9c059cc220582b9de0b
Shamidow <pannier.thibault@gmail.com>
2020-05-27 10:53:10 +0200

    Je crois j'ai réparé le nuage

--- Assets/Scripts/ParticleDMGtest.cs
+++ Assets/Scripts/ParticleDMGtest.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ParticleDMGtest : MonoBehaviour
 {
-    
+
     public ParticleSystem part;
     public Image HealthBar;
     public Image HealthBarG;
-    public GameObject[] Cough;
-    private int randomInt;
-    private int Timer = 8;
+    
     void Start()
     {
         part = GetComponent<ParticleSystem>();
-        randomInt = Random.Range(0, Cough.Length);
-        StartCoroutine(RandomCough());
+       
     }
 
     void OnParticleCollision(GameObject other)
@@ -31,33 +28,4 @@ public class ParticleDMGtest : MonoBehaviour
         }
     }
 
-    int GetRandom(int count)
-    {
-        return Random.Range(0, count);
-    }
-
-    IEnumerator RandomCough()
-    {
-        while (Timer != 0)
-        {
-            randomInt = GetRandom(Cough.Length);
-           
-
-           Destroy(Instantiate(Cough[randomInt]), 5f);
-            Timer--;
-           
-            yield return new WaitForSeconds(5f);
-        }
-
-        while (Timer <= 0)
-        {
-            randomInt = GetRandom(Cough.Length);
-          
-
-            Destroy(Instantiate(Cough[randomInt]), 5f);
-
-            yield return new WaitForSeconds(5f);
-        }
-    }
 }
-
