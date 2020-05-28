a8019b42b556b7913880910d916844771c880473
EtCtulu <bertrandemilien17@gmail.com>
2020-05-28 11:12:34 +0200

    Rechargement du menu

---
+++ Assets/Scripts/Backtomenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backtomenu : MonoBehaviour
+{
+    private float counterbacktomenu = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterbacktomenu = counterbacktomenu + Time.deltaTime;
+
+        if(counterbacktomenu >= 41f)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
