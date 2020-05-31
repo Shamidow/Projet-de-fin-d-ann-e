0547c01404dbc3b396c2491f21e23bdafda54829
EtCtulu <bertrandemilien17@gmail.com>
2020-05-31 19:22:40 +0200

    BAIROZ

---
+++ Assets/Scripts/pressanykey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressanykey : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject start;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            start.SetActive(false);
+            menu.SetActive(true);
+        }
+    }
+}
