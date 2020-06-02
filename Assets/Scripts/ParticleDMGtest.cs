c82fcaca9ad6467f6dea2be910548eb4bdb793a6
Shamidow <pannier.thibault@gmail.com>
2020-06-02 02:48:37 +0200

    Aller hop le DmgScreen il revient hop dodo

--- Assets/Scripts/ParticleDMGtest.cs
+++ Assets/Scripts/ParticleDMGtest.cs
@@ -2,60 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ParticleDMGtest : MonoBehaviour
 {
 
     public ParticleSystem part;
-    
+    public Image HealthBar;
+    public Image HealthBarG;
     public GameObject dmgScreen;
-    public Image Imagedmg;
-    public GameObject FpsController;
     private float timer = 0f;
     private bool timerB = false;
-   
+
     void Start()
     {
         part = GetComponent<ParticleSystem>();
-        Imagedmg = dmgScreen.GetComponent<Image>();
-       
-        
+
     }
 
     private void Update()
     {
-        /*if (timerB == true)
+        if (timerB == true)
         {
             timer = timer + Time.deltaTime;
         }
-        if(timer >= 0.25f)
+        if (timer >= 0.25f)
         {
+            Debug.Log("frérot la vérité je m'en vais");
             dmgScreen.SetActive(false);
             timer = 0f;
             timerB = false;
-        }*/
-        
-        
+        }
     }
 
     void OnParticleCollision(GameObject other)
     {
         if (other.tag == "Player")
         {
-            
+            dmgScreen.SetActive(true);
             Debug.Log(other.tag);
             PlayerHP.hp = PlayerHP.hp - 0.20f;
-            
-            var colores = Imagedmg.color;
-            colores.a += 0.003f;
-            Imagedmg.color = colores;
-            Debug.Log(dmgScreen.GetComponent<Image>().color);
            
-            //Debug.Log("Nique ta race" + PlayerHP.hp);
-            //timerB = true;
+            Debug.Log("Nique ta race" + PlayerHP.hp);
+            timerB = true;
 
         }
     }
-    
-    
+
 }
